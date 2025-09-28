e87daf062bb8915bf992f3914ec32524bda2c58d
GADIK187 <vlad.dvoretskiy2011@gmail.com>
2025-09-28 19:55:29 +0300

    add txt file

---
+++ login.js
@@ -0,0 +1,11 @@
+const mainContainer = document.querySelector('.main-container');
+const signupLink = document.querySelector('.signup-link');
+const loginLink = document.querySelector('.login-link');
+
+signupLink.onclick = () => {
+    mainContainer.classList.add('active');
+}
+
+loginLink.onclick = () => {
+    mainContainer.classList.remove('active');
+}
